94bb77b4cb2db33014154b7485b0c33776b12518
marciomazeu <mazeu911@gmail.com>
2024-03-06 21:21:29 -0500

    update everything

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+const menuItens = document.querySelectorAll('.portfolio__menu a[href^="#"]');
+
+console.group(menuItens);
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+    console.log(item);
+});
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const section = document.querySelector(id).offsetTop;
+    console.log(id);
+    console.log(section);
+
+    window.scroll({
+        top: section,
+        behavior: 'smooth'
+    })
+}
